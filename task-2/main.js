42a14414b338bd5c8c35289e3303260472e20fe5
FarruxAhmadov <105041270+FarruxAhmadov@users.noreply.github.com>
2022-07-05 11:57:19 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,16 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+prompt.get("input", function (err, result) {
+  let eded = parseInt(result.input);
+  var result = false;
+  for (i = 2; i < eded; i++) {
+    if (eded % i === 0) {
+      result = true;
+      console.log("No");
+      break;
+    }
+  }
+  if (result === false) {
+    console.log("Yes");
+  }
 });
